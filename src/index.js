09e32af31749cfa007df6b92034eb45717d804e8
Jeff Winning <25329856+swiftwinning@users.noreply.github.com>
2019-12-15 13:47:32 -0500

    Fetches user data from API and creates basic table

--- src/index.js
+++ src/index.js
@@ -2,5 +2,75 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<h1>Random Users App</h1>, document.getElementById('root'));
+class UserList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {results: [{
+		    dob: {age: 0},
+			gender: "",
+			location: {country: ""},
+			name: {first: "", last: ""}
+	    }] };
+    }
+  
+  componentDidMount(){
+	  
+	  const URL =
+		  'https://randomuser.me/api/?results=20&nat=us,ca&inc=gender,name,location,dob';
+	  
+	  fetch(URL)
+		  .then(res => res.json())
+		  .then(res => {
+		      this.setState({results: res.results});
+		      console.log("Fetch Results...");
+		      console.log(this.state.results);
+		   })	  
+	}
+	
+	render() {
+	    return (
+	        <table>
+	            <tr>
+	                <th>Gender</th>
+	                <th>First Name</th>
+	                <th>Last Name</th>
+	                <th>Country</th>
+	                <th>Birthday</th>
+	            </tr>
+	            {
+					this.state.results.map(
+					 user =>
+							<UserRow user={user} />
+					)
+				}
+	        </table>
+	    )
+	}
+}
 
+class UserRow extends React.Component {
+	render() {
+	    return (
+	        <tr>
+	            <UserDataCell data={this.props.user.gender} />
+	            <UserDataCell data={this.props.user.name.first} />
+	            <UserDataCell data={this.props.user.name.last} />
+	            <UserDataCell data={this.props.user.location.country} />
+	            <UserDataCell data={this.props.user.dob.age} />
+	        </tr>
+	    )
+	}
+}
+
+class UserDataCell extends React.Component {
+  render() {
+	    return (
+	        <td>{this.props.data}</td>
+	    )
+	}
+}
+
+ReactDOM.render(
+  <UserList />,
+  document.getElementById('root')
+);
