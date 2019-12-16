c710e63c5704a071993d3770e028b31e2f215f93
Jeff Winning <25329856+swiftwinning@users.noreply.github.com>
2019-12-16 17:13:04 -0500

    Updates

---
+++ precache-manifest.b721e15f222b98513961fc7f00c39645.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5643c4df9e88c8475a277523d3ab99",
+    "url": "/brown-cis-random-users-app/index.html"
+  },
+  {
+    "revision": "129e7acc743276c66f43",
+    "url": "/brown-cis-random-users-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "129e7acc743276c66f43",
+    "url": "/brown-cis-random-users-app/static/js/2.48ecff6e.chunk.js"
+  },
+  {
+    "revision": "bfb19a4219c76289b554e6db627c1aec",
+    "url": "/brown-cis-random-users-app/static/js/2.48ecff6e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "31f4987320c7a4994bd4",
+    "url": "/brown-cis-random-users-app/static/js/main.30835728.chunk.js"
+  },
+  {
+    "revision": "ecadd7a54830d3bb74b2",
+    "url": "/brown-cis-random-users-app/static/js/runtime-main.09d9fc49.js"
+  }
+]);
